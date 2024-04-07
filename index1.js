250673761cae6a894698450f9718b7769e09e939
junkinatsui <dptgagtma0573@gmail.com>
2024-04-07 23:23:41 +0900

    復習

--- index1.js
+++ index1.js
@@ -1,33 +1,82 @@
+console.log("この文字がコンソール表示されます。");
+let age = 25;
+const name = 'Alice';
+
+console.log(name + 'is' + age + 'years old.');
+
+age = 26;
+
+console.log(name + 'will be' + age + 'next year.')
 
 
-let weather = "晴れ";
+age = 20;
+console.log(age);
 
-switch (weather) {
-    case "晴れ":
-        console.log("今日の天気は晴れです。");
-        break;
-    case "曇り":
-        console.log("今日は曇りです。");
-        break;
-    case "雨":
-        console.log("今日は雨です。");
-        break;
-    default:
-        console.log("天気が不明です。");
+
+
+let colors = ["赤","緑","青"];
+console.log(colors[0]);
+
+let person = {
+    firstName: "Bob",
+    lastName: "Smith",
+    age: 25
+};
+console.log(person.firstName);
+
+let greet = function(name) {
+    return "こんにちわ、" + name + "さん！";
+};
+console.log(greet("Alice"));
+let itemPrice1 = 20.99;
+let itemPrice2 = 35.50;
+let shoppingCost = 5.00;
+let totalcost = itemPrice1 + itemPrice2 +shoppingCost;
+console.log("Total Cost: $" + totalcost.toFixed(2));
+
+let number1 = 300;
+number1 += 200;
+
+console.log(number1);
+
+
+let number = 100;
+
+
+
+
+
+if (number > 200) {
+  console.log("numberは200より大きいです。");
+} else if (number > 50) {
+  console.log("numberは50より大きいです。");
+} else {
+  console.log("numberは50より小さいです。");
 }
 
-function sumSelectNumber(number1,number2) {
-    let resultSum = 0;
-    resultSum = number1 + number2;
-    return resultSum;
+
+let gender = "男";
+
+if (gender === "男" && age >= 20) {
+    console.log("20歳以上の男性です");
 }
-console.log(sumSelectNumber(200,400));
 
 
-let result = sumSelectNumber(100,200);
-console.log(result); 
+let number2 = "100";
+let boolean_flag = false;
 
-$(document).ready(function(){
-    $(document).行たい処理など
-});
+if (number1 > 100) {
+    number1 =200;
+} else if (number1 !== number2) {
+    boolean_flag = true;
+    number1 += 300;
+} else {
+    boolean_flag = true;
+    number1 = 300;
+}
 
+if (number1 >= 400 && boolean_flag) {
+    console.log("出力結果A");
+} else {
+    console.log("出力結果B");
+}
