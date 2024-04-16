a0fadfd9059f7fc51d1ff93a847fc07fc1cab9d8
junkinatsui <dptgagtma0573@gmail.com>
2024-04-16 23:45:44 +0900

    calculator 模写

---
+++ index2.js
@@ -0,0 +1,53 @@
+
+
+let weather = "晴れ";
+
+switch (weather) {
+    case "晴れ":
+        console.log("今日の天気は晴れです。");
+        break;
+    case "曇り":
+        console.log("今日は曇りです。");
+        break;
+    case "雨":
+        console.log("今日は雨です。");
+        break;
+    default:
+        console.log("天気が不明です。");
+}
+
+function sumSelectNumber(number1,number2) {
+    let resultSum = 0;
+    resultSum = number1 + number2;
+    return resultSum;
+}
+console.log(sumSelectNumber(200,400));
+
+
+let result = sumSelectNumber(100,200);
+console.log(result); 
+
+$(document).ready(function(){
+    $(document).行たい処理など
+});
+
+class Calculator {
+    static OPERATORS = ["+", "-", "*", "/"];
+    static MULTIPLY_OPERATOR = "*";
+    static DIVISION_OPERATOR = "/";
+    static DECIMAL_POINT = ".";
+  }
+  
+  function selectNumber(number) {
+    if (display.innerHTML == "0" || display.innerHTML == "00") {
+      return display.innerHTML = number;
+    }
+  
+    display.innerHTML += number;
+  }
+  
+  function addNumber() {
+    if (isLastCharOperator() || isLastCharDecimalPoint()) return;
+  
+    display.innerHTML += " + ";
+  }
