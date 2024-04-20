e842dc333cfa28321cfa36bbc01cf72c7aa84f66
junkinatsui <dptgagtma0573@gmail.com>
2024-04-20 11:34:16 +0900

    stopwatch

--- index2.js
+++ index2.js
@@ -128,3 +128,107 @@ class Calculator {
   
 
   const calculator = new Calculator();  
+
+
+  class StopWatch {
+    constructor() {
+      this.hour = 0;
+      this.minute = 0;
+      this.second = 0;
+      this.millisecond = 0;
+    }
+  
+    start() {
+      this.timer = setInterval(this.count.bind(this), 100);
+    }
+  
+    stop() {
+      clearInterval(this.timer);
+    }
+  
+    reset() {
+      this.hour = this.minute = this.second = this.millisecond = 0;
+      this.updateNumbers();
+  
+      clearInterval(this.timer);
+    }
+  
+    count() {
+      this.millisecond++
+  
+      if (this.millisecond >= 10) {
+        [this.millisecond, this.second] = this.carryToNextDigit(this.millisecond, this.second)
+        this.updateNumbers();
+      }
+  
+      if (this.second >= 60) {
+        [this.second, this.minute] = this.carryToNextDigit(this.second, this.minute)
+        this.updateNumbers();
+      }
+  
+      if (this.minute >= 60) {
+        [this.minute, this.hour] = this.carryToNextDigit(this.minute, this.hour)
+        this.updateNumbers();
+      }
+  
+      this.millisecond.toString();
+      $millisecond.text(this.millisecond)
+    }
+  
+    updateNumbers() {
+      $hour.text(this.hour);
+      $minute.text(this.minute);
+      $second.text(this.second);
+      $millisecond.text(this.millisecond);
+    }
+  
+    carryToNextDigit(current, next) {
+      current = 0;
+      next += 1
+  
+      return [current, next]
+    }
+  }
+  
+  function makeDisabled(element) {
+    element.prop('disabled', true);
+  }
+  
+  function makeEnabled(element) {
+    element.prop('disabled', false);
+  }
+  
+  function start() {
+    makeDisabled($startButton);
+    makeEnabled($stopButton);
+    makeEnabled($resetButton);
+  
+    stopWatch.start();
+  }
+  
+  function stop() {
+    makeEnabled($startButton);
+    makeDisabled($stopButton);
+    makeEnabled($resetButton);
+  
+    stopWatch.stop();
+  }
+  
+  function reset() {
+    makeEnabled($startButton);
+    makeDisabled($stopButton);
+    makeDisabled($resetButton);
+  
+    stopWatch.reset();
+  }
+  
+  const $startButton = $("#js-start-button");
+  const $stopButton = $("#js-stop-button");
+  const $resetButton = $("#js-reset-button");
+  
+  const $hour = $("#js-hour");
+  const $minute = $("#js-minute");
+  const $second = $("#js-second");
+  const $millisecond = $("#js-millisecond");
+  
+  const stopWatch = new StopWatch();
